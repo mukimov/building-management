dd0846c05950f55ae522703d5e573c0ce7f4a04d
Shukhrat Mukimov <mukimov@gmail.com>
2017-10-18 15:31:55 +0500

    rename and refactor home controller

---
+++ BuildingManagement/Controllers/BuildingsController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace BuildingManagement.Controllers {
+	public class BuildingsController : Controller {
+		public ActionResult Index() {
+			return View();
+		}
+	}
+}
